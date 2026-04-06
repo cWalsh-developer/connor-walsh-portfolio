18889deadbeb11dc9913d31119a8d0a04eac3e66
Connor <connor_w_98@hotmail.com>
2026-04-06 05:16:01 +0100

    style: update status bar color in VSCode settings and format Vite config for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "connor-walsh-portfolio",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  assetsInclude: ['**/*.svg', '**/*.csv'],
-})
+  assetsInclude: ["**/*.svg", "**/*.csv"],
+});
